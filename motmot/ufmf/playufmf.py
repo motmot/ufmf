a25d5c809a3b9f00121b19c138712c294db2df52
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2008-10-21 22:02:46 +0000

    attempt to install playufmf as GUI App
    git-svn-id: https://code.astraw.com/kookaburra/trunk/ufmf@2815 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ motmot/ufmf/playufmf.py
@@ -0,0 +1,29 @@
+import motmot.FlyMovieFormat.playfmf as playfmf
+import motmot.ufmf.ufmf as ufmf
+from optparse import OptionParser
+import sys
+
+def main():
+    usage = '%prog FILE [options]'
+
+    parser = OptionParser(usage)
+    (options, args) = parser.parse_args()
+
+    if len(args)<1:
+        parser.print_help()
+        return
+
+    filename = args[0]
+
+    if (sys.platform.startswith('win') or
+        sys.platform.startswith('darwin')):
+        kws = dict(redirect=True,filename='playfmf.log')
+    else:
+        kws = {}
+    app = playfmf.MyApp(**kws)
+    flymovie = ufmf.FlyMovieEmulator(filename)
+    app.OnNewMovie(flymovie)
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
