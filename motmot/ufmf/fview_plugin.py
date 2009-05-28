4cf4b53a70ac42f08aecf33df0d2caa57904d43d
Andrew Straw <strawman@astraw.com>
2009-05-27 18:10:22 -0700

    use simpler Event model

--- motmot/ufmf/fview_plugin.py
+++ motmot/ufmf/fview_plugin.py
@@ -10,33 +10,13 @@ import motmot.realtime_image_analysis.trealtime_image_analysis as tra
 import motmot.FastImage.FastImage as FastImage
 import threading
 
-class CrossThreadEvent(traits.HasTraits):
-    """like threading.Event class, with button to set the event"""
-    _threading_event = traits.Any(transient=True)
-    fire_event = traits.Button
-
-    is_set = traits.Any(transient=True)
-    set = traits.Any(transient=True)
-    clear = traits.Any(transient=True)
-
-    traits_view = View(Group(Item(name='fire_event',show_label=False)))
-
-    def __init__(self,*args,**kwargs):
-        super(CrossThreadEvent,self).__init__(*args,**kwargs)
-        self._threading_event = threading.Event()
-        self.is_set = self._threading_event.is_set
-        self.set = self._threading_event.set
-        self.clear = self._threading_event.clear
-
-    def _fire_event_fired(self):
-        self._threading_event.set()
-
 class UFMFFviewSaver(traited_plugin.HasTraits_FViewPlugin):
     plugin_name = 'ufmf saver'
     saving = traits.Bool(False)
     start_saving = traits.Button()
     stop_saving = traits.Button()
-    clear_and_take_BG = traits.Instance(CrossThreadEvent,transient=True)
+    clear_and_take_BG = traits.Button()
+    _clear_take_event = traits.Any(transient=True)
     bg_Nth_frame = traits.Int(100)
     use_roi2 = traits.Bool(True)
     realtime_analyzer = traits.Instance( tra.TraitedRealtimeAnalyzer )
@@ -45,14 +25,17 @@ class UFMFFviewSaver(traited_plugin.HasTraits_FViewPlugin):
                              Item(name='stop_saving',show_label=False),
                              Item(name='bg_Nth_frame'),
                              Item(name='use_roi2'),
-                             Item(name='clear_and_take_BG',style='custom'),
+                             Item(name='clear_and_take_BG'),
                              Item(name='realtime_analyzer',
                                   style='custom'),
                              )
                        )
     def __init__(self,*args,**kwargs):
         super(UFMFFviewSaver,self).__init__(*args,**kwargs)
-        self.clear_and_take_BG = CrossThreadEvent()
+        self._clear_take_event = threading.Event()
+
+    def _clear_and_take_BG_fired(self):
+        self._clear_take_event.set()
 
     def camera_starting_notification(self,cam_id,
                                      pixel_format=None,
@@ -69,9 +52,8 @@ class UFMFFviewSaver(traited_plugin.HasTraits_FViewPlugin):
         fibuf = FastImage.asfastimage(buf)
         l,b = buf_offset
         lbrt = l, b, l+fibuf.size.w-1, b+fibuf.size.h-1
-        if self.clear_and_take_BG.is_set():
-            print 'clear_and_take_BG'
-            self.clear_and_take_BG.clear()
+        if self._clear_take_event.is_set():
+            self._clear_take_event.clear()
         self.realtime_analyzer.do_work( fibuf, timestamp, framenumber,
                                         self.use_roi2)
         return draw_points, draw_linesegs
