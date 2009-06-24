449be4265d0469a05901b3ad6002b59cc2320ead
flightlab <flightlab@cam1.(none)>
2009-06-23 18:55:37 -0700

    bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ kws=dict(
 
 setup(name='motmot.ufmf',
       description='micro-fmf (.ufmf) format library',
-      version='0.2.2',
+      version='0.2.3',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       namespace_packages = ['motmot'],
