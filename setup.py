4b10ce413f9c8ae73ad92f68b4fb270e38d0059f
Andrew Straw <strawman@astraw.com>
2009-07-07 12:50:26 -0700

    bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ kws=dict(
 
 setup(name='motmot.ufmf',
       description='micro-fmf (.ufmf) format library',
-      version='0.2.5',
+      version='0.2.6',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       namespace_packages = ['motmot'],
