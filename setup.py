46ae3422b07250de40e042b3eb34dabef8b26a7c
Andrew Straw <strawman@astraw.com>
2020-04-08 11:37:47 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
-kws = dict()
-
 setup(
     name="motmot.ufmf",
-    description="micro-fmf (.ufmf) format library",
     version="0.4.1",
+    description="micro-fmf (.ufmf) format library",
     author="Andrew Straw",
     author_email="strawman@astraw.com",
     namespace_packages=["motmot"],
